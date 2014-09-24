52e476eddcb31be152ac2ac89baa2051888af9b9
vtt <vtt@a.a>
2014-09-25 00:00:38 +0400

    changed multireader receive

--- Interprocess/vtt/interprocess/implementation/Single Writer Multi Reader Pipe.hpp
+++ Interprocess/vtt/interprocess/implementation/Single Writer Multi Reader Pipe.hpp
@@ -34,6 +34,7 @@ namespace n_implementation
 			#pragma region Fields
 
 			volatile long m_ec_readers;
+			volatile long m_magic;
 
 			#pragma endregion
 		};
@@ -55,6 +56,7 @@ namespace n_implementation
 		public: auto Read(_Out_writes_bytes_opt_(bc_buffer_capacity) char * p_buffer, _In_ const size_t bc_buffer_capacity, _In_ const int timeout_msec) -> size_t
 		{
 			auto & buffer = m_shared_memory.Obtain<t_Buffer>();
+			volatile const long initial_magic = buffer.m_magic;
 			auto p_ec_readers = &(buffer.m_ec_readers);
 			// variables used in atomic operations must be aligned by 32 bits (4 bytes that is)
 			assert(0 == (reinterpret_cast<ptrdiff_t>(p_ec_readers) % 4));
@@ -63,22 +65,26 @@ namespace n_implementation
 			{
 				assert(0 < *p_ec_readers);
 				auto ec_readers = InterlockedCompareExchange(p_ec_readers, 0, 1);
-				auto this_was_not_last_reader = (1 != ec_readers);
-				if(this_was_not_last_reader)
-				{
-					InterlockedDecrement(p_ec_readers);
-				}
-				else
+				if(1 == ec_readers)
 				{
 					this_->m_flag.Reset(); // last reader resets event
+				}
+				else if(1 < ec_readers)
+				{
+					InterlockedDecrement(p_ec_readers);
 				}
 			}
 			BOOST_SCOPE_EXIT_END;
 			auto bc_read = static_cast<size_t>(0);
-			auto const wait_result = m_flag.Timed_Wait(timeout_msec);
-			if(false != wait_result)
+			m_flag.Timed_Wait(timeout_msec);
+			//auto const wait_result = m_flag.Timed_Wait(timeout_msec);
+			//if(false != wait_result)
 			{
-				bc_read = buffer.Retrieve_Data(p_buffer, bc_buffer_capacity);
+				volatile const long new_magic = buffer.m_magic;
+				if(new_magic != initial_magic)
+				{
+					bc_read = buffer.Retrieve_Data(p_buffer, bc_buffer_capacity);
+				}
 			}			
 			return(bc_read);
 		}
@@ -90,6 +96,9 @@ namespace n_implementation
 			auto & buffer = m_shared_memory.Obtain<t_Buffer>();
 			buffer.Clear();
 			buffer.Store(p_data, bc_data);
+			// variables used in atomic operations must be aligned by 32 bits (4 bytes that is)
+			assert(0 == (reinterpret_cast<ptrdiff_t>(&buffer.m_magic) % 4));
+			InterlockedIncrement(&buffer.m_magic);
 			m_flag.Set();
 		}
 	};
